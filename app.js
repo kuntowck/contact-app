0beae092f1dc41c09da14a206024a184d5f30679
kuntowck <kunto312002@gmail.com>
2024-04-26 09:50:29 +0700

    polymorphism files

--- app.js
+++ app.js
@@ -1,47 +1,13 @@
-const fs = require('fs');
-const { resolve } = require('path');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout 
-});
-
-// membuat directory data
-const dir = './data';
-if (!fs.existsSync(dir)){
-   fs.mkdirSync(dir);
-}
-// membuat file contacts.json
-const dataPath = './data/contacts.json';
-if (!fs.existsSync(dataPath)){ 
-   fs.writeFileSync(dataPath, '[]', 'utf-8');
-}
-
-const tulisPertanyaan = (pertanyaan) => {
-   return new Promise((resolve, reject) => {
-      rl.question(pertanyaan, nama => {
-         resolve(nama);
-      });
-   });
-};
+const contacts = require('./contacts');
+// const {tulisPertanyaan, simpanContacts} = require('./contacts'); //object distractring
 
 const main = async () => {
-   const nama = await tulisPertanyaan('Masukkan nama: ');
-   const email = await tulisPertanyaan('Masukkan email: ');
-   const noHp = await tulisPertanyaan('Masukkan noHp: ');
+   const nama = await contacts.tulisPertanyaan('Masukkan nama: ');
+   const email = await contacts.tulisPertanyaan('Masukkan email: ');
+   const noHp = await contacts.tulisPertanyaan('Masukkan noHp: ');
 
-   const contact = {nama, email, noHp};
-   const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-   const json = JSON.parse(fileBuffer);
-
-   json.push(contact);
-
-   fs.writeFileSync('data/contacts.json', JSON.stringify(json));
-
-   console.log('Terima kasih sudah menginputkan data.');
-
-   rl.close();
+   contacts.simpanContacts(nama, email, noHp);
 } 
 
 main();
+
